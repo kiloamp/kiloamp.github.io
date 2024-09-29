00f86be6150e97241851f5293ec8d7767ea18763
kiloamp <emailuser.com@gmail.com>
2024-09-29 23:00:15 +0200

    pushing new UX! and also updated images

--- slideshow.js
+++ slideshow.js
@@ -1,28 +1,47 @@
-let slideIndex = 1;
-showSlides(slideIndex, "A");
-showSlides(slideIndex, "B");
-showSlides(slideIndex, "C");
-showSlides(slideIndex, "D");
-showSlides(slideIndex, "E");
-showSlides(slideIndex, "F");
-showSlides(slideIndex, "G");
-showSlides(slideIndex, "H");
+let slideIndexes = {
+  A: 1,
+  B: 1,
+  C: 1,
+  D: 1,
+  E: 1,
+  F: 1
+};
+
+showSlides(slideIndexes.A, "A");
+showSlides(slideIndexes.B, "B");
+showSlides(slideIndexes.C, "C");
+showSlides(slideIndexes.D, "D");
+showSlides(slideIndexes.E, "E");
+showSlides(slideIndexes.F, "F");
 
 function plusSlides(n, X) {
-  showSlides(slideIndex += n, X);
+  slideIndexes[X] += n;
+  showSlides(slideIndexes[X], X);
 }
 
 function currentSlide(n, X) {
-  showSlides(slideIndex = n, X);
+  slideIndexes[X] = n;
+  showSlides(slideIndexes[X], X);
 }
 
 function showSlides(n, X) {
-  let i;
   let slides = document.getElementsByClassName("slide_" + X);
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  if (n > slides.length) {
+    slideIndexes[X] = 1;
+  }
+  if (n < 1) {
+    slideIndexes[X] = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndexes[X] - 1].style.display = "block";
 }
+
+setInterval(function() {
+  const image_reel = document.getElementById('image_reel');
+  const current = image_reel.children[0];  // First image (current)
+  image_reel.appendChild(current);         // Move current to the end
+  current.style.display = 'none';         // Hide current
+  image_reel.children[0].style.display = 'block';  // Show next
+}, 1500);
